f84be11caa51ad9885df0dcc8b9c9dfd77bb0f53
Jarred Taylor <jarredt@gmail.com>
2017-08-23 14:27:20 -0700

    Change boilerplate to new content

--- index.android.js
+++ index.android.js
@@ -15,38 +15,24 @@ import {
 export default class LetsDoLunch extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}}>
+          <Text style={styles.text}>
+            {"Let's Do Lunch"}
+          </Text>
+        </View>
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  text: {
+    fontSize: 50,
+    color: 'black',
+    textAlign: 'center'
   },
 });
 
